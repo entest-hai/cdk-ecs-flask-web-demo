20ec3f138896f7087fe009e280d2e2ec940e9b56
EC2 Default User <ec2-user@ip-10-0-0-155.ap-southeast-1.compute.internal>
2022-07-14 11:44:56 +0000

    tested

--- lib/cdk-ecs-web-stack.ts
+++ lib/cdk-ecs-web-stack.ts
@@ -1,16 +1,139 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  aws_ec2,
+  aws_ecr,
+  aws_ecs,
+  aws_elasticloadbalancingv2,
+  Duration,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+interface CdkEcsWebStackProps extends StackProps {
+  vpcId: string;
+  vpcName: string
+}
 
 export class CdkEcsWebStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: CdkEcsWebStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // lookup an existed vpc 
+    const vpc = aws_ec2.Vpc.fromLookup(
+      this,
+      "LookUpVpc",
+      {
+        vpcId: props.vpcId,
+        vpcName: props.vpcName
+      }
+    )
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkEcsWebQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ecs cluster
+    const cluster = new aws_ecs.Cluster(this, "EcsClusterFlaskApp", {
+      vpc: vpc,
+      clusterName: "EcsClusterFlaskApp",
+      containerInsights: true,
+      enableFargateCapacityProviders: true,
+      
+    });
+
+    // ecs task definition
+    const task = new aws_ecs.FargateTaskDefinition(
+      this,
+      "FlaskTaskDefinition",
+      {
+        family: "latest",
+        cpu: 2048,
+        memoryLimitMiB: 4096,
+        runtimePlatform: {
+          operatingSystemFamily: aws_ecs.OperatingSystemFamily.LINUX,
+          cpuArchitecture: aws_ecs.CpuArchitecture.X86_64,
+        },
+      }
+    );
+
+    // container
+    const container = task.addContainer("FlaskAppContainer", {
+      containerName: "FlaskAppContainer",
+      memoryLimitMiB: 4096,
+      memoryReservationMiB: 4096,
+      stopTimeout: Duration.seconds(120),
+      startTimeout: Duration.seconds(120),
+      environment: {
+        FHR_ENV: "DEPLOY",
+      },
+      image: aws_ecs.ContainerImage.fromEcrRepository(
+        aws_ecr.Repository.fromRepositoryName(
+          this,
+          "FlaskAppRepository",
+          "flask-app-demo"
+        ),
+      ),
+      portMappings: [{ containerPort: 8080 }],
+    });
+
+    // service
+    const service = new aws_ecs.FargateService(this, "FlaskService", {
+      vpcSubnets: {
+        subnetType: aws_ec2.SubnetType.PUBLIC
+      },
+      assignPublicIp: true,
+      cluster: cluster,
+      taskDefinition: task,
+      desiredCount: 2,
+      capacityProviderStrategies: [
+        {
+          capacityProvider: "FARGATE",
+          weight: 1,
+        },
+        {
+          capacityProvider: "FARGATE_SPOT",
+          weight: 0,
+        },
+      ],
+    });
+
+    // scaling on cpu utilization
+    const scaling = service.autoScaleTaskCount({
+      maxCapacity: 4,
+      minCapacity: 1,
+    });
+
+    scaling.scaleOnMemoryUtilization("CpuUtilization", {
+      targetUtilizationPercent: 50,
+    });
+
+    // application load balancer
+    const alb = new aws_elasticloadbalancingv2.ApplicationLoadBalancer(
+      this,
+      "AlbForEcs",
+      {
+        loadBalancerName: "AlbForEcsDemo",
+        vpc: vpc,
+        internetFacing: true,
+      }
+    );
+
+    // add listener
+    const listener = alb.addListener("Listener", {
+      port: 80,
+      open: true,
+      protocol: aws_elasticloadbalancingv2.ApplicationProtocol.HTTP,
+    });
+
+    // add target
+    listener.addTargets("EcsService", {
+      port: 8080,
+      targets: [
+        service.loadBalancerTarget({
+          containerName: "FlaskAppContainer",
+          containerPort: 8080,
+          protocol: aws_ecs.Protocol.TCP,
+        }),
+      ],
+      healthCheck: {
+        timeout: Duration.seconds(10),
+      },
+    });
   }
 }
--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,6 @@
 # Hai Tran 13 JUN 2022
 # Setup flask
 
-from flask import Flask
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -13,4 +12,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
